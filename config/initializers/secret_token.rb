24a2af9a4da8e905e4707c47b92f6c1055af4d91
mikegmoll <mike.g.moll@gmail.com>
2012-10-24 02:27:05 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+THJ::Application.config.secret_token = 'fe6f80f6d239d02a535015a7d25c9a474029521b61b6ff823cc087bd3867dc7a2815b263932c43c12056d9d484394d0316b7329cec8461aa29953f76bfbcf297'
